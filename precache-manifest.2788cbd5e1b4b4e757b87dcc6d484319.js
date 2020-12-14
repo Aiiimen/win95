a339f727bd5e30441f82c80891352bd9aa5ed6ee
Aiiimen <aimenbenfika@gmail.com>
2020-12-14 23:33:30 +0000

    Add files via upload

---
+++ precache-manifest.2788cbd5e1b4b4e757b87dcc6d484319.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "837074b9101d24d837f7fab39a47a010",
+    "url": "/win95/index.html"
+  },
+  {
+    "revision": "3bf76a39ccdfde28c0c9",
+    "url": "/win95/static/css/main.27be5be3.chunk.css"
+  },
+  {
+    "revision": "e92f64c0e081e1a98a1d",
+    "url": "/win95/static/js/2.d502a6fb.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/win95/static/js/2.d502a6fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bf76a39ccdfde28c0c9",
+    "url": "/win95/static/js/main.d9f49314.chunk.js"
+  },
+  {
+    "revision": "19469817f8ffb272b206",
+    "url": "/win95/static/js/runtime-main.b06a81d1.js"
+  },
+  {
+    "revision": "f3c3e33dcf674968847fa16ee6a8e332",
+    "url": "/win95/static/media/blue_rivets_preview.f3c3e33d.png"
+  },
+  {
+    "revision": "262dcc04e30852ba70b98bb69b4501e6",
+    "url": "/win95/static/media/bubbles-wallpaper.262dcc04.png"
+  },
+  {
+    "revision": "b60ff06367e1b7657c7fcb6d42cb3501",
+    "url": "/win95/static/media/bubbles_preview.b60ff063.png"
+  },
+  {
+    "revision": "589dd733e5a4dd847a3ab1ab91107237",
+    "url": "/win95/static/media/carved_stone_preview.589dd733.png"
+  },
+  {
+    "revision": "d50e10042befed3049f2d85d7f7ab4e0",
+    "url": "/win95/static/media/craved-stone-wallpaper.d50e1004.png"
+  },
+  {
+    "revision": "6030dea694df725e1b9331bcae97bc59",
+    "url": "/win95/static/media/loading.6030dea6.gif"
+  },
+  {
+    "revision": "95487bc0561069f3191bebc2eafe8ac7",
+    "url": "/win95/static/media/loading.95487bc0.gif"
+  },
+  {
+    "revision": "122969d507596f1d19fff52d6ec5d3cf",
+    "url": "/win95/static/media/metal-links-wallpaper.122969d5.png"
+  },
+  {
+    "revision": "605d275b60c950fd249b4b1a0e05e748",
+    "url": "/win95/static/media/metal_links_preview.605d275b.png"
+  },
+  {
+    "revision": "aec960d878246bd3b6b4d0961319102c",
+    "url": "/win95/static/media/red-block-wallpaper.aec960d8.png"
+  },
+  {
+    "revision": "28055d31cd43c67c1c4386134761cf3f",
+    "url": "/win95/static/media/red_blocks_preview.28055d31.png"
+  },
+  {
+    "revision": "3ca20491cac0e8b7cf373bfe3647664f",
+    "url": "/win95/static/media/straw-mat-wallpaper.3ca20491.png"
+  },
+  {
+    "revision": "acbb490817ac427359d0c1574811f934",
+    "url": "/win95/static/media/straw_mat_preview.acbb4908.png"
+  }
+]);
